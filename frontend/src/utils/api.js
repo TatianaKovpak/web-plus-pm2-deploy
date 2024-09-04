8d6a3233ec3178843895124d51ebaf992a085363
unknown <tanechka.kovpak@yandex.ru>
2024-09-04 14:51:48 +0300

    1

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.tkovpak.student.nomorepartiesco.ru/');
+  const api = new Api('https://api.tkovpak.student.nomorepartiesco.ru');
 
   export default api;
